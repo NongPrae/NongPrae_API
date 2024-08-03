f33650dbc4da3c79f635db6511c235d9f3cf10ea
Rafky <pharitachaikong80@gmail.com>
2024-08-03 12:02:42 +0700

    index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+//sc by Rafky
+
+const {
+    default: makeWASocket,
+    useMultiFileAuthState
+} = require("@whiskeysockets/baileys");
+
+const readline = require("readline")
+const Pino = require("pino");
+
+const usePairingCode = true
+
+const question = (text) => {
+  const rl = readline.createInterface({
+input: process.stdin,
+output: process.stdout
+  });
+  return new Promise((resolve) => {
+rl.question(text, resolve)
+  })
+};
+
+(async function connectToWhatsApp() {
+    const { state, saveCreds } = await useMultiFileAuthState("auth");
+    const bot = makeWASocket({
+      logger: Pino({ level: "silent" }),
+      browser: [ "Ubuntu", "Chrome", "20.0.04" ],
+      auth: state,
+      printQRInTerminal: !usePairingCode,
+      defaultQueryTimeoutMs: undefined,
+      syncFullHistory: false
+    });
+    if(usePairingCode && !bot.authState.creds.registered) {
+		const phoneNumber = await question('Masukan Nomer Yang Aktif Awali Dengan 62:\n');
+		const code = await bot.requestPairingCode(phoneNumber.trim())
+		console.log(`Pairing code: ${code}`)
+	}
+    bot.ev.on("connection.update", (c) => {
+        const { connection, lastDisconnect } = c;
+        if(connection === "close") {
+          console.log(lastDisconnect);
+          connectToWhatsApp();
+        }
+        if(connection === "open") {
+          console.log(bot.user.id.split(":")[0]);
+        }
+    });
+    bot.ev.on("creds.update", saveCreds);
+})();
